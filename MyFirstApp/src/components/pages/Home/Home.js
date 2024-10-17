0d96d774b4b710398e80d14aadcec40b7481b63d
Yetukurikeerthi <keerthiyetukuri@gmail.com>
2024-10-17 10:53:15 -0400

    Changed navigation Text

--- MyFirstApp/src/components/pages/Home/Home.js
+++ MyFirstApp/src/components/pages/Home/Home.js
@@ -1,16 +1,14 @@
-
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View>
-      <Text>Home Screen</Text>
+      <Text>Keerthi Home Screen</Text>
       <TouchableOpacity onPress={() => navigation.navigate("Hometwo")}>
         <Text>Go to different page</Text>
       </TouchableOpacity>
     </View>
   );
 };
-
 export default HomeScreen;
